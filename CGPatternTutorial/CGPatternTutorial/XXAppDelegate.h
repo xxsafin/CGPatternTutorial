2782b6cad565092189e4ca71b9018444040939ea
xuxian <xuxian05@qq.com>
2014-04-22 14:32:40 +0800

    first commit

---
+++ CGPatternTutorial/CGPatternTutorial/XXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXAppDelegate.h
+//  CGPatternTutorial
+//
+//  Created by Xu Xian on 14-4-22.
+//  Copyright (c) 2014年 Xu Xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CGPatternTutorial/CGPatternTutorial/XXViewController.h
@@ -0,0 +1,13 @@
+//
+//  XXViewController.h
+//  CGPatternTutorial
+//
+//  Created by Xu Xian on 14-4-22.
+//  Copyright (c) 2014年 Xu Xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXViewController : UIViewController
+
+@end
